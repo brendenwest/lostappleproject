b27ece50bd8a4cbb1a48251460576f74f42ff4cb
Scott H <zenwattage@gmail.com>
2021-10-20 17:19:59 -0700

    Added views for logo, added logo and Images folder, and Text view for home info

--- NavPages/Home.js
+++ NavPages/Home.js
@@ -1,12 +1,48 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 
 function Home() {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Home Page!</Text>
-    </View>
+    <View style={styles.container}>
+      <View style={styles.logocontainer}>
+        <Image style={styles.logo} source={require('../Images/lapLogo.jpg')} />
+      </View>
+
+      <Text style={styles.text}>The Whitman County Historical Society Lost Apple Project seeks to identify and preserve heritage apple trees and orchards in the Inland Empire.</Text>
+
+    </View >
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderWidth: 5,
+    padding: 10,
+    margin: 10,
+  },
+  logoContainer: {
+    flex: 0.3,
+    backgroundColor: 'blue',
+    borderWidth: 5,
+
+  },
+  logo: {
+    borderWidth: 5,
+    width: 345,
+    height: 360,
+  },
+  text: {
+    flex: 2,
+    padding: 10,
+    borderWidth: 5,
+    backgroundColor: '#b22222',
+    textAlign: 'center',
+    marginTop: 10,
+    fontSize: 25,
+    fontWeight: 'bold',
+
+  }
+});
 
 export default Home;
