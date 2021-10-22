9002a74b029c9cf9ae054072d209b8ea8f0e435e
ZenSorcere <zen.sorcere@gmail.com>
2021-10-22 16:28:26 -0700

    initial firebase packages and attempted connect

--- NavPages/Apples.js
+++ NavPages/Apples.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 function About() {
+  firestore().collection('apples').get()
+  .then(querySnapshot => {
+    console.log('Total apples: ', querySnapshot.size);
+    querySnapshot.forEach(documentSnapShot => {
+      console.log('apple: ', documentSnapshot.id, documentSnapshot.data());
+    });
+  });
+  
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Apples Page!</Text>
