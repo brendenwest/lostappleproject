6f490917e8c06555721c89c28d9cbd88eb1c13b8
ZenSorcere <zen.sorcere@gmail.com>
2021-10-30 12:14:33 -0700

    initial firebase info display successful test

--- NavPages/Apples.js
+++ NavPages/Apples.js
@@ -1,11 +1,49 @@
-import React from 'react';
-import {Text, View, Image, ScrollView} from 'react-native';
+import React,  { useState, useEffect } from 'react';
+import {Text, View, Image, ScrollView, ActivityIndicator, FlatList, StatusBar} from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import Header from '../shared/Header';
 import styles from '../StyleSheet/AppleList';
 
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+const renderItem = ({ item }) => (
+  <Item title={item.name} />
+);
+
 function Apples() {
-  const appleCollection = firestore().collection('apples');
+  const [loading, setLoading] = useState(true); // Set loading to true on component mount
+  const [appleList, setApples] = useState([]); // Initial empty array of users
+
+  useEffect(() => {
+    const appleCollection = firestore()
+    .collection('apples')
+    .onSnapshot(querySnapshot => {
+      const appleList = [];
+
+      querySnapshot.forEach(documentSnapshot => {
+        appleList.push({
+          ...documentSnapshot.data(),
+          key: documentSnapshot.id,
+        });
+      });
+
+      setApples(appleList);
+      setLoading(false);
+    });
+
+    // Unsubscribe from events when no longer in use
+    return () => appleCollection();
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  /* **************************************************** 
   function onResult(QuerySnapshot) {
     console.log('Got Apple collection result.');
   }
@@ -14,8 +52,12 @@ function Apples() {
     console.error(error);
   }
  
-  appleCollection.onSnapshot(onResult, onError);
+  appleCollection.onSnapshot(onResult, onError); 
+  ******************************************************** */
 
+  
+
+  /* *****************************************************
   firestore().collection('apples').get()
   .then(querySnapshot => {
     console.log('Total apples: ', querySnapshot.size);
@@ -23,17 +65,25 @@ function Apples() {
       console.log('apple: ', documentSnapshot.data().name);
     });
   });
+  ******************************************************** */
   
   return (
-    <ScrollView /*style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}*/>
+    <>
       <Header />
-      <Text style={styles.centerText}>Apples Page!</Text>
       <Image
         style={styles.applePlaceholder}
         source={require('../Images/apple-3155.png')}
       />
-    </ScrollView>
+      <FlatList
+        data={appleList}
+        renderItem={renderItem}
+        key={item => item.id} /* keyExtractor={item => item.id}*/
+      />
+    </>
+    
   );
 }
+
+
 
 export default Apples;
--- StyleSheet/AppleList.js
+++ StyleSheet/AppleList.js
@@ -1,53 +1,37 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, StatusBar} from 'react-native';
 
 export default StyleSheet.create({
+  container: {
+    flex: 1,
+    /* marginTop: StatusBar.currentHeight || 0, */
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 10,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+
+  title: {
+    fontSize: 12,
+  },
+
   centerText: {
     textAlign: 'center',
     fontSize: 20,
-    padding: 11,
+    padding: 10,
     fontWeight: 'bold',
     color: 'black',
-  },
-
-  aboutImg: {
-    marginTop: 10,
-    width: 350,
-    height: 210,
-    backgroundColor: 'grey',
-    alignSelf: 'center',
   },
 
   applePlaceholder: {
     width: 50,
     height: 50,
     backgroundColor: 'green',
-    alignSelf: 'center',
+    /* alignSelf: 'center', */
     borderWidth: 2,
     borderColor: 'black',
     borderRadius: 10,
   },
 
-  textHeading: {
-    fontSize: 18,
-    textAlign: 'center',
-    fontWeight: 'bold',
-    color: '#142044',
-    marginBottom: 2,
-  },
-
-  contact: {
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: 'brown',
-    marginTop: 5,
-    paddingTop: 5,
-    marginRight: 10,
-    marginBottom: 10,
-    width: 230,
-    height: 130,
-    backgroundColor: 'skyblue',
-    textAlign: 'center',
-    alignSelf: 'flex-end',
-    color: 'black',
-  },
 });
