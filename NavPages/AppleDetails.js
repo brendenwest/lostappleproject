3a1ee944b3d0c9145e68d5e960cdd2c965a51dc0
ZenSorcere <zen.sorcere@gmail.com>
2021-11-15 16:29:41 -0800

    apply initial css styles to appledetails page

--- NavPages/AppleDetails.js
+++ NavPages/AppleDetails.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Image} from 'react-native';
 import appleList from './Apples';
+import Header from '../shared/Header';
+import styles from '../StyleSheet/AppleDetailsStyle';
 
 const AppleDetails = ({navigation, route}) => {
   apDeets = route.params.id;
   //console.log('Params: ' + JSON.stringify(apDeets));
 
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>{apDeets.name}</Text>
-      <Text>Found: {apDeets.found ? apDeets.found : '???'}</Text>
-      <Text>{apDeets.location}</Text>
+    <View style={{flex: 1, alignItems: 'center'}}>
+      <Header />
+      <View style={styles.title} >
+        <Text style={styles.titleText}>{apDeets.name}</Text>
+      </View>
+      <Image
+          style={styles.applePlaceholder}
+          source={require('../Images/apple-3155.png')}
+        />
+      <Text style={styles.locText}>Found: {apDeets.found ? apDeets.found : '???'}</Text>
+      <Text style={styles.locText}>{apDeets.location}</Text>
+      <Text style={styles.descText}>Description of the apple</Text>
 
       {/* ADD FUTURE APPLE IMAGE, APPLE DESC, POSSIBLE EMBEDDED MAP FOR LOCATION?  */}
     </View>
