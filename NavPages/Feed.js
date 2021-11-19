436bffa2701acd5620e9867ea058ab0ffa45d55e
Scott H <zenwattage@gmail.com>
2021-11-18 21:46:45 -0800

    working FB linking button added

--- NavPages/Feed.js
+++ NavPages/Feed.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { Text, View, ScrollView } from 'react-native';
+import { Text, View, ScrollView, Button, Linking, StyleSheet } from 'react-native';
 import Header from '../shared/Header';
 
 function Feed() {
   return (
-    <ScrollView>
-      <Header />
-    </ScrollView>
+    <>
+      <View>
+        <Header />
+      </View>
+
+      <ScrollView style={{ margin: 10 }}>
+        <Button
+          title="Open the Lost Apple Project Facebook page"
+          color="#4267B2"
+          onPress={() =>
+            Linking.openURL('https://m.facebook.com/lostappleproject/?__tn__=%2Cg',)
+          }
+        />
+      </ScrollView>
+    </>
   );
 }
+
 
 export default Feed;
